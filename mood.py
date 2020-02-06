d78fa2a1e6d28eb6b8f71cb70dd073fd0a02fc71
Buzz <Buzz627@gmail.com>
2020-02-06 11:47:19 -0500

    return most recent mood for test user

--- mood.py
+++ mood.py
@@ -7,3 +7,6 @@ collection=db.moods
 def saveMood(data):
 	collection.insert_one(data)
 
+def getMood(user):
+	data=collection.find({"user":user}, collation={"locale":"en", "strength":2}, sort=[("time", -1)])
+	return list(data)
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 # from flask_api import status
 from datetime import datetime
 import json
-from mood import saveMood
+from mood import saveMood, getMood
 
 
 app = Flask(__name__)
@@ -15,8 +15,14 @@ def mood():
 		return jsonify({"message": "user in needed"}), 400
 	if "mood" not in data:
 		return jsonify({"message": "mood in needed"}), 400
-	print({"mood": data["mood"], "user":data["user"], "time": datetime.now()})
+	saveMood({"mood": data["mood"], "user":data["user"], "time": datetime.now()})
 	return jsonify(), 200
 
 
-
+@app.route("/mood", methods=['GET'])
+def showMood():
+	data=getMood("buzz")
+	print("here", data)
+	result=data[0]
+	del result["_id"]
+	return jsonify(result)
