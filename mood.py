9207ac8de721d4260b20126809016e24503d6b92
Buzz <Buzz627@gmail.com>
2020-02-11 10:24:35 -0500

    added streak and percentile stuff

--- mood.py
+++ mood.py
@@ -1,6 +1,8 @@
 import pymongo
+import datetime
 
 client=pymongo.MongoClient()
+# client=pymongo.MongoClient("mongodb://mongo:27018")
 db = client.moodAPI
 collection=db.moods
 
@@ -10,3 +12,61 @@ def saveMood(data):
 def getMood(user):
 	data=collection.find({"user":user}, collation={"locale":"en", "strength":2}, sort=[("time", -1)])
 	return list(data)
+
+def getStreak(user):
+	data=collection.find({"user":user}, {"time":1}, collation={"locale":"en", "strength":2}, sort=[("time", -1)])
+	start=data.next()["time"]
+	streak=0
+	for i in range(data.count()-1):
+		end=data.next()["time"]
+		if start.date()-end.date()==datetime.timedelta(1):
+			streak+=1
+		elif start.date()-end.date()==datetime.timedelta(0):
+			pass
+		else:
+			break
+		
+		start=end
+
+	return streak+1
+
+def longestStreak(user):
+	data=collection.find({"user":user}, {"time":1}, collation={"locale":"en", "strength":2}, sort=[("time", -1)])
+	start=data.next()["time"]
+	streak=[]
+	curStreak=0
+	for i in range(data.count()-1):
+
+		end=data.next()["time"]
+		if start.date()-end.date()==datetime.timedelta(1):
+
+			curStreak+=1
+		elif start.date()-end.date()==datetime.timedelta(0):
+			pass
+		else:
+			streak.append(curStreak+1)
+			curStreak=0
+		
+		start=end
+	else:
+		streak.append(curStreak+1)
+
+	return max(streak)
+
+
+def getPercentile(user):
+	bestStreak=longestStreak(user)
+	names=collection.distinct("user", {})
+	streaks=[]
+	for name in names:
+		if name.lower()==user.lower():
+			continue
+
+		streaks.append(longestStreak(name))
+	less=sum(list(map(lambda x: x<bestStreak, streaks)))
+	return less/(len(streaks)+1)
+
+
+if __name__=="__main__":
+	print(getPercentile("buzz"))
+
--- server.py
+++ server.py
@@ -1,28 +1,44 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 # from flask_api import status
 from datetime import datetime
 import json
-from mood import saveMood, getMood
+from mood import *
 
 
 app = Flask(__name__)
+# app.run(host="0.0.0.0", port=5000)
+# app.run()
 
 
 @app.route("/mood", methods=['POST'])
 def mood():
 	data = request.json
 	if "user" not in data:
-		return jsonify({"message": "user in needed"}), 400
+		abort(400)
+		# return jsonify({"message": "user in needed"}), 400
+
 	if "mood" not in data:
-		return jsonify({"message": "mood in needed"}), 400
-	saveMood({"mood": data["mood"], "user":data["user"], "time": datetime.now()})
+		abort(400)
+		# return jsonify({"message": "mood in needed"}), 400
+
+	saveMood({"mood": data["mood"], "user":data["user"], "time": datetime.datetime.now()})
 	return jsonify(), 200
 
 
 @app.route("/mood", methods=['GET'])
 def showMood():
-	data=getMood("buzz")
+	jsonBody=request.json
+	data=getMood(jsonBody["user"])
 	print("here", data)
 	result=data[0]
 	del result["_id"]
+	result["streak"]=getStreak(jsonBody["user"])
+	percentile=getPercentile(jsonBody["user"])
+	if percentile>=0.5:
+		result["percentile"]=percentile*100
 	return jsonify(result)
+
+
+
+if __name__=="__main__":
+	print(datetime.fromtimestamp(1581009183))
